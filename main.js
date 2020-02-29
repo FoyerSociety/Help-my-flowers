cdb25f5f2d598d5f0b3f1d11988670a1f5b4cc0a
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2020-02-29 15:14:04 +0300

    Deplyement dans electron pret

---
+++ main.js
@@ -0,0 +1,23 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegrations: true
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('http://localhost:1107/index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+
+app.on('ready', createWindow)
---
+++ run.py
@@ -0,0 +1,6 @@
+import eel 
+
+eel.init("static/")
+
+eel.start(mode="electron", port=1107)
+
