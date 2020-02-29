6651e1bc425123b2b6d096b5e48dbd409b97a262
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2020-02-29 15:42:20 +0300

    class simulation arduino

--- run.py
+++ run.py
@@ -1,6 +1,35 @@
-import eel 
+import eel
+import random
 
 eel.init("static/")
+
+
+class Data():
+    __hum_sensor, __lum_sensor = 0, 0
+
+    def __init__(self):
+        """
+            Simulation des données par l'Arduino pour 
+                le mode de developpement sans capteur
+                                                    """
+
+
+    @property
+    def getHum(self):
+        self.__change_value(hum=True)
+        return self.__hum_sensor
+
+
+    @property
+    def getLum(self):
+        self.__change_value(lum=True)
+        return self.__lum_sensor
+
+
+    def __change_value(self, hum=False, lum=False):
+        if lum: self.__lum_sensor = random.randint(40, 80)
+        if hum: self.__hum_sensor = random.randint(40, 80)
+
 
 eel.start(mode="electron", port=1107)
 
